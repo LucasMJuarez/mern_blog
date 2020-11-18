4603f1dc4baac34c0f8b077784a06f2aaf28b693
LucasMJuarez <lumas89@gmail.com>
2020-11-18 14:56:45 -0300

    first commit mern

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+//Routes
+app.use('/posts',postRoutes)
+
+//Middlewares
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://lumas147:lumas89@lumas89.y8ptm.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
