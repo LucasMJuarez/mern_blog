4ba322eeea1defa6dbe24f70ab1c38b12ff28e5d
LucasMJuarez <lumas89@gmail.com>
2020-11-18 16:16:33 -0300

    seccond commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import recuerdos from './images/recuerdos.jpg';
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Recuerdos
+          </Typography>
+          <img src={recuerdos} alt="recuerdos" height='500'/>
+      </AppBar>
+      <Grow in>
+    <Container>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} >
+            <Grid item xs={12} sm={7}>
+                <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form/>
+            </Grid>
+        </Grid>
+    </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
