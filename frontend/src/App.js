fefa60466854a91314913b6a45f503028e0776f9
LucasMJuarez <lumas89@gmail.com>
2020-11-18 18:26:44 -0300

    added styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import recuerdos from './images/recuerdos.jpg';
+import recuerdos from "./images/recuerdos.jpg";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import useStyles  from './styles'
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">
-            Recuerdos
-          </Typography>
-          <img src={recuerdos} alt="recuerdos" height='500'/>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Recuerdos
+        </Typography>
+        <img className={classes.image} src={recuerdos} alt="recuerdos" height="500" />
       </AppBar>
       <Grow in>
-    <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3} >
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xs={12} sm={7}>
-                <Posts/>
+              <Posts />
             </Grid>
             <Grid item xs={12} sm={4}>
-                <Form/>
+              <Form />
             </Grid>
-        </Grid>
-    </Container>
+          </Grid>
+        </Container>
       </Grow>
     </Container>
   );
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Post from './Post/Post.js'
+import useStyles  from './styles'
+
 const Posts = () => {
+    const classes = useStyles();
+
     return (
         <>
         <h1>Posts</h1>
